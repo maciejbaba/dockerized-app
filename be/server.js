7e6dec14efb169ff96eaacd710891bb06d0b59e8
Maciej Baba <maciek2baba2@gmail.com>
2024-10-16 23:06:35 +0200

    add mongoose to the server

--- be/server.js
+++ be/server.js
@@ -1,10 +1,12 @@
 import express from "express";
+import mongoose from "mongoose";
 const app = express();
 app.use(express.json());
+mongoose.connect("mongodb://localhost:27017").catch((err) => console.log(err));
 const PORT = 3000;
-app.get("/washing-machines", (req, res) => {
-  res.send("test");
+app.get("/washing-machines", (_, res) => {
+    res.send("test");
 });
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+    console.log(`Server is listening on port ${PORT}`);
 });
