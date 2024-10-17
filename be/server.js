6671474a4380f7aeb03ca04b0b8611383e248fc4
Maciej Bąba <maciek2baba2@gmail.com>
2024-10-17 17:44:01 +0200

    improve docker-compose, add necessary libs to backend, fix tailwindcss, add init script to mongodb

--- be/server.js
+++ be/server.js
@@ -1,15 +1,40 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 import express from "express";
-import mongoose from "mongoose";
+import { MongoClient } from "mongodb";
+import cors from "cors";
+require("dotenv").config();
 const app = express();
 app.use(express.json());
+app.use(cors());
 const dbUrl = process.env.MONGODB_URL;
-if (dbUrl) {
-    mongoose.connect(dbUrl).catch((err) => console.log(err));
+if (!dbUrl) {
+    throw new Error("No database URL found in the environment variables");
 }
+const client = new MongoClient(dbUrl);
+const dbName = "washing-machines-db";
+const collectionName = "washing-machines-collection";
 const PORT = 3000;
-app.get("/washing-machines", (_, res) => {
-    res.send("test");
-});
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-});
+let db;
+let collection;
+client
+    .connect()
+    .then((client) => {
+    db = client.db(dbName);
+    collection = db.collection(collectionName);
+    app.listen(PORT, () => {
+        console.log(`Server is listening on port ${PORT}`);
+    });
+})
+    .catch((error) => console.error(error));
+app.get("/washing-machines", (_, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const allMachines = yield collection.find({}).toArray();
+    res.send({ data: allMachines });
+}));
--- be/server.ts
+++ be/server.ts
@@ -1,22 +1,74 @@
 import express, { Request, Response } from "express";
-import mongoose from "mongoose";
+import { Collection, MongoClient } from "mongodb";
+import cors from "cors";
+require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
 
+app.use(cors());
+
 const dbUrl = process.env.MONGODB_URL;
 
-if (dbUrl) {
-  mongoose.connect(dbUrl).catch((err) => console.log(err));
+if (!dbUrl) {
+  throw new Error("No database URL found in the environment variables");
 }
+
+const client = new MongoClient(dbUrl);
+
+const dbName = "washing-machines-db";
+const collectionName = "washing-machines-collection";
 
 const PORT = 3000;
 
-app.get("/washing-machines", (_: Request, res: Response) => {
-  res.send("test");
-});
+type EnergyClass = "A" | "B" | "C";
+type Capacity = 8 | 9 | 10.5;
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+type Features =
+  | "Drzwi AddWash™"
+  | "Panel AI Control"
+  | "Silnik inwerterowy"
+  | "Wyświetlacz elektroniczny";
+
+interface IProduct {
+  image: string;
+  code: string;
+  name: string;
+  color: string;
+  capacity: Capacity;
+  dimensions: string;
+  features: Features[];
+  energyClass: EnergyClass;
+  price: {
+    value: number;
+    currency: string;
+    installment: {
+      value: number;
+      period: number;
+    };
+    validFrom: Date;
+    validTo: Date;
+  };
+}
+
+let db;
+let collection: Collection<IProduct>;
+
+client
+  .connect()
+  .then((client) => {
+    db = client.db(dbName);
+    collection = db.collection(collectionName);
+
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}`);
+    });
+  })
+  .catch((error) => console.error(error));
+
+app.get("/washing-machines", async (_: Request, res: Response) => {
+  const allMachines: IProduct[] = await collection.find({}).toArray();
+
+  res.send({ data: allMachines });
 });
